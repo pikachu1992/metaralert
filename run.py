b32833bbe423071dab70149306a877fe0f858cc8
pikachu1992 <tmavicente@gmail.com>
2019-02-27 03:32:37 +0100

    add conditions

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ def get_metar_with_negative_temp(airport):
     else:
         windgust = 0
 
-    if (temp <= 0 or dwpt <= 0 or windspeed <= 30 or windgust <= 30):
+    if (temp <= 0 or dwpt <= 0 or windspeed >= 30 or windgust >= 30):
         return metar['Raw-Report']
 
 airports = ['LPPT', 'LPPR', 'LPFR', 'LPMA']
